a8aed276abe2bbae10396fc98e1924d587410a1a
Aayush Rijal <aayush.rijal99@gmail.com>
2022-05-18 14:06:03 +1000

    Laptop screen done

--- app/src/header.php
+++ app/src/header.php
@@ -33,7 +33,7 @@
 						</a>
 					</div>
 					<div class="col-6 d-md-none"><?= renderImg("master-builders.png", "logo") ?></div>
-					<div class="col-12 col-md-auto col-xl-7 col-xxl-6 pt-2 pt-md-0">
+					<div class="col-12 col-md-auto col-xl-7 pt-2 pt-md-0">
 						<div class="row justify-content-center justify-content-lg-between no-gutters align-items-center">
 							<div class="col-7 d-none d-lg-block">
 								<?= renderImg("master-builders.png", "logo") ?>
